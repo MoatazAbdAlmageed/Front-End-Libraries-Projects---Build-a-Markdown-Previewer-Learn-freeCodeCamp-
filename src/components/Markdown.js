f2043637f140f0c7022772acedc3af2fc3cd9bfc
moataz <moataz.mohammady@gmail.com>
2018-07-11 15:48:52 +0200

    ALLOWS LINE BREAKS WITH RETURN BUTTON && INSERTS target="_blank" INTO HREF TAGS (required for codepen links)

--- src/components/Markdown.js
+++ src/components/Markdown.js
@@ -3,6 +3,19 @@ import React, {Component} from 'react';
 
 import marked from 'marked';
 
+
+// ALLOWS LINE BREAKS WITH RETURN BUTTON
+marked.setOptions({
+    breaks: true,
+});
+
+// INSERTS target="_blank" INTO HREF TAGS (required for codepen links)
+const renderer = new marked.Renderer();
+renderer.link = function (href, title, text) {
+    return `<a target="_blank" href="${href}">${text}` + '</a>';
+}
+
+
 export class Markdown extends Component {
 
     constructor(props) {
